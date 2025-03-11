c6d8db5b459861b1934f7350eefac419ad38b47b
張惇頤 <tunyic@chouatsogaismbp.lan>
2025-03-11 16:34:23 -0700

    skeleton for openAI service and endpoint

--- src/main/kotlin/com/example/demo/OpenAIService.kt
+++ src/main/kotlin/com/example/demo/OpenAIService.kt
@@ -1,22 +1,81 @@
 package com.example.demo
 
+import com.aallam.openai.client.OpenAI
+import com.aallam.openai.api.chat.ChatCompletionRequest
+import com.aallam.openai.api.model.ModelId
+import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ChatRole
+import com.aallam.openai.api.chat.ChatCompletion
+import com.aallam.openai.api.chat.ChatResponseFormat
+import com.aallam.openai.api.chat.JsonSchema
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.Json
+
 class OpenAIService {
 
-    val openAI = OpenAI(apiKey)
-    val chatCompletionRequest = ChatCompletionRequest(
-        model = ModelId("gpt-3.5-turbo"),
-        messages = listOf(
-            ChatMessage(
-                role = ChatRole.System,
-                content = "You are a helpful assistant!"
+    companion object {
+        val responseFormat: String = "{\n" +
+                "    \"type\": \"object\",\n" +
+                "    \"properties\": {\n" +
+                "      \"result\": {\n" +
+                "        \"type\": \"array\",\n" +
+                "        \"items\": {\n" +
+                "          \"type\": \"object\",\n" +
+                "          \"properties\": {\n" +
+                "            \"month\": {\n" +
+                "              \"type\": \"string\"\n" +
+                "            },\n" +
+                "            \"reason\": {\n" +
+                "              \"type\": \"string\",\n" +
+                "              \"enum\": [\n" +
+                "                \"good weather\",\n" +
+                "                \"less crowded\",\n" +
+                "                \"festival\"\n" +
+                "              ]\n" +
+                "            }\n" +
+                "          },\n" +
+                "          \"required\": [\n" +
+                "            \"month\",\n" +
+                "            \"reason\"\n" +
+                "          ],\n" +
+                "          \"additionalProperties\": false\n" +
+                "        }\n" +
+                "      }\n" +
+                "    },\n" +
+                "    \"required\": [\n" +
+                "      \"result\"\n" +
+                "    ],\n" +
+                "    \"additionalProperties\": false\n" +
+                "  }"
+    }
+
+    suspend fun getTravelSuggestion(rawData: String): ChatCompletion {
+
+        val chatCompletionRequest = ChatCompletionRequest(
+            model = ModelId("gpt-4o-mini"),
+            messages = listOf(
+                ChatMessage(
+                    role = ChatRole.User,
+                    content = rawData
+                )
             ),
-            ChatMessage(
-                role = ChatRole.User,
-                content = "Hello!"
+            responseFormat = ChatResponseFormat(
+                type = "json_schema",
+                jsonSchema= JsonSchema(
+                    schema = Json.decodeFromString(JsonObject.serializer(), responseFormat),
+                    strict =  true,
+                    name = "travel_advice"
+                )
             )
         )
+
+        val completion = openAI.chatCompletion(chatCompletionRequest)
+        return completion
+    }
+
+
+    val openAI = OpenAI(
+        token = "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",
     )
-    val completion: ChatCompletion = openAI.chatCompletion(chatCompletionRequest)
-    // or, as flow
-    val completions: Flow<ChatCompletionChunk> = openAI.chatCompletions(chatCompletionRequest)
+
 }
