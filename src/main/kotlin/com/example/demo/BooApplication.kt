e86e3cb75edf8ea450eeea31b0559d5bff179967
張惇頤 <tunyic@chouatsogaismbp.lan>
2025-04-10 23:00:27 -0700

    set up token from properties

--- src/main/kotlin/com/example/demo/BooApplication.kt
+++ src/main/kotlin/com/example/demo/BooApplication.kt
@@ -1,9 +1,11 @@
 package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(Configurations::class)
 class BooApplication
 
 fun main(args: Array<String>) {
